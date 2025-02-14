133796d5ee2bb06544eb96deb6dfce16d321039f
Roshan Thapa <roshanrays236@gmail.com>
2025-02-14 09:27:21 +0545

    working Template

---
+++ app/src/main/java/com/example/memento_gram/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.memento_gram
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (FirebaseAuth.getInstance().currentUser==null)
+            startActivity(Intent(this, SignUpActivity::class.java))
+            else
+                startActivity(Intent(this, HomeActivity2::class.java))
+            finish()
+        }, 3000)
+    }
+}
